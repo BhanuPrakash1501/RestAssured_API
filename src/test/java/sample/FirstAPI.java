d7e2588747b7b2775b6fef89faf8cd579767f30c
BhanuPrakash1501 <revanthshetty80@gmail.com>
2025-09-17 23:55:58 +0530

    First Api

--- src/test/java/sample/FirstAPI.java
+++ src/test/java/sample/FirstAPI.java
@@ -1,7 +1,15 @@
 package sample;
 
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+
 public class FirstAPI {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        given().log().all().queryParams("key","qaclick123").header("content-Type","application/json")
+                .body("{\n  \"location\": {\n    \"lat\": -38.383494,\n    \"lng\": 33.427362\n  },\n  \"accuracy\": 50,\n  \"name\": \"Frontline house\",\n  \"phone_number\": \"(+91) 983 893 3937\",\n  \"address\": \"29, side layout, cohen 09\",\n  \"types\": [\n    \"shoe park\",\n    \"shop\"\n  ],\n  \"website\": \"http://google.com\",\n  \"language\": \"French-IN\"\n}")
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().statusCode(200);
     }
 }
